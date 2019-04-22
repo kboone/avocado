0973f6daf1d3789a76ddefcc4fd626e96d89a9b4
Kyle Boone <kboone@berkeley.edu>
2019-04-22 11:02:05 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='avocado',
+    version='0.1',
+    description='Photometric Classification of Astronomical Transients and '
+    'Variables With Biased Spectroscopic Samples',
+    url='http://github.com/kboone/avocado',
+    author='Kyle Boone',
+    author_email='kboone@berkeley.edu',
+    license='BSD',
+    packages=['avocado'],
+)
